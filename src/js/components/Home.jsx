c98f2f59131f1465266838e17a1b1c7332cc2311
Santigarrido28 <Santiagop2001@outlook.com>
2026-08-29 15:51:32 +0000

    First commit

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,56 @@
-import React from "react";
+iimport React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const [task, setTask] = useState("");
+	const [tasks, setTasks] = useState([]);
+	const deleteTask = (indexToDelete) => {
+		setTasks(
+			tasks.filter((task, index) => {
+				return index !== indexToDelete;
+			})
+		);
+	};
+	const addTask = () => {
+		if (task.trim() !== "") {
+			setTasks([...tasks, task]);
+			setTask("");
+		}
+	};
+	return (
+		<div className="container">
+			<h1>Lista de Tareas</h1>
+			<div className="todo-box">
+				<input
+					type="text"
+					placeholder="¿Qué tareas tienes pendientes?"
+					value={task}
+					onChange={(event) => setTask(event.target.value)}
+					onKeyDown={(event) => {
+						if (event.key === "Enter") {
+							addTask();
+						}
+					}}
+				/>
+				<ul>
+					{tasks.length === 0 ? (
+						<li className="task-item">No hay tareas pendientes, añadir tareas.</li>
+					) : (
+						tasks.map((task, index) => (
+							<li key={index} className="task-item">
+								{task}
+								<i
+									className="fa-regular fa-circle-xmark delete-btn"
+									onClick={() => deleteTask(index)}
+								></i>
+							</li>
+						))
+					)}
+				</ul>
+				<p>
+					{tasks.length} {tasks.length === 1 ? "tarea pendiente" : "tareas pendientes"}
+				</p>
+			</div>
 		</div>
 	);
 };
